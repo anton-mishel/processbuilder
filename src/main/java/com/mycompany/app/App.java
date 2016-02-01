06125c143cd5d2ada5f1ed7677d41d343a47396d
anton-mishel <anton.mishel@gmail.com>
2016-02-01 11:24:22 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,45 @@
+package com.mycompany.app;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class App {
+  public static void main(String[] args) throws InterruptedException ,
+    IOException {
+    List<String> list = new ArrayList<String>();
+    //list.add("ls");
+    //list.add("/tmp");
+    list.addAll(Arrays.asList(args));
+ 
+    ProcessBuilder pb = new ProcessBuilder(list);
+    
+    System.out.println("Run echo command");
+    Process process = pb.start();
+    int errCode = process.waitFor();
+    System.out.println("Echo command executed, any errors? " + (errCode == 0 ? "No" : "Yes"));
+    System.out.println("Echo Output:\n" + output(process.getInputStream()));  
+  }
+
+  private static String output(InputStream inputStream) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new InputStreamReader(inputStream));
+      String line = null;
+      while ((line = br.readLine()) != null) {
+        sb.append(line + System.getProperty("line.separator"));
+      }
+    } finally {
+      br.close();
+    }
+    return sb.toString();
+  }
+}
